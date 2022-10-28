8ac7e88171a2368d503b06c3f73a1024b1dd1d1c
Mustafoyev <mmustafoyev9@gmail.com>
2022-10-28 15:09:42 +0500

    commit message

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -1,8 +1,21 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InterfaceCreator {
 
     public Operation<Integer> divideBy(Integer divider) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return new Operation<Integer>() {
+            List<Integer> list = new ArrayList<>();
+
+            @Override
+            public List<Integer> apply(List<Integer> arg) {
+                for (int number : arg) {
+                    list.add(number / 3);
+                }
+                return list;
+            }
+        };
     }
 }
